66844965af8dfd011661097abaf295a3d8fa9c87
Akshat Jain <akshatjain325@gmail.com>
2025-04-27 23:44:32 +0530

    Add Hotel entity and configure RestTemplate for user service integration

---
+++ UserService/UserService/src/main/java/com/user/service/entities/Hotel.java
@@ -0,0 +1,21 @@
+package com.user.service.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Hotel {
+    private String Id;
+    private String name;
+    private String location;
+    private String about;
+}
--- UserService/UserService/src/main/java/com/user/service/services/impl/UserServiceImpl.java
+++ UserService/UserService/src/main/java/com/user/service/services/impl/UserServiceImpl.java
@@ -1,17 +1,28 @@
 package com.user.service.services.impl;
 
+import com.netflix.discovery.converters.Auto;
+import com.user.service.entities.Hotel;
+import com.user.service.entities.Rating;
 import com.user.service.entities.User;
 import com.user.service.exceptions.ResourceNotFoundException;
 import com.user.service.repositories.UserRepository;
 import com.user.service.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Autowired
     private UserRepository userRepository;
@@ -31,7 +42,23 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found with user id: "+ userId));
+
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found with user id: "+ userId));
+        Rating[] ratingsForUser = restTemplate.getForObject("http://localhost:8083/ratings/users/" + userId, Rating[].class);
+        List<Rating> ratings = Arrays.stream(ratingsForUser).toList();
+
+        List<Rating> ratingsWithHotel = ratings.stream().map(rating -> {
+
+            ResponseEntity<Hotel> hotelEntity = restTemplate.getForEntity("http://localhost:8082/hotels/" + rating.getHotelId(), Hotel.class);
+            Hotel hotel = hotelEntity.getBody();
+            rating.setHotel(hotel);
+
+            return rating;
+        }).collect(Collectors.toList());
+
+        user.setRatings(ratingsWithHotel);
+
+        return user;
     }
 
     @Override
